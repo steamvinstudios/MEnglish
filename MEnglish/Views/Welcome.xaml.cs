feec3b4e38b4406df260c944806a50658154e87d
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-03-28 20:52:37 +0300

    Global Cleaning

--- MEnglish/Views/Welcome.xaml.cs
+++ MEnglish/Views/Welcome.xaml.cs
@@ -1,39 +1,28 @@
-﻿using Microsoft.UI.Xaml.Controls;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Networking.BackgroundTransfer;
-using Windows.Storage;
-using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace MEnglish
+namespace MEnglish.Views
 {
     /// <summary>
     /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
     /// </summary>
-    public sealed partial class Dictionary : Page
+    public sealed partial class Welcome : Page
     {
-        public Dictionary()
+        public Welcome()
         {
             this.InitializeComponent();
         }
