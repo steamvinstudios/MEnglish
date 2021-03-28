6ea364ca9926e216e2ca1b2299d3df0c08326c57
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-03-28 18:11:51 +0300

    Очистка станицы словаря Подготовка к рефакторингу

--- MEnglish/Dictionary.xaml.cs
+++ MEnglish/Dictionary.xaml.cs
@@ -33,78 +33,9 @@ namespace MEnglish
     /// </summary>
     public sealed partial class Dictionary : Page
     {
-        public int StackMargin { get; set; }
-        public Words Words { get; set; } = new Words();
-        Image ttipContentImage = new Image()
-        {
-            MaxWidth = 120,
-            MaxHeight = 120,
-            Source = new BitmapImage 
-            {
-                UriSource = new Uri("ms-appx:///ArrayPics/book.jpg")
-            } 
-        };
         public Dictionary()
         {
             this.InitializeComponent();
-        }
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ttip.IsOpen = true;
-            var lView = sender as ListView;
-            var currentItem = (Word)lView.SelectedItem;
-            ttip.Title = currentItem.OriginalWord;
-            ttip.Subtitle = currentItem.WordTranslation.ToString();
-            ttip.Target = mainAB;
-        }
-
-        // Handle text change and present suitable items
-        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-        {
-            var suitableItems = new List<string>();
-            var splitText = sender.Text.ToLower().Split(" ");
-            foreach (var word in Words.WordsArray)
-            {
-                var found = splitText.All((key) =>
-                {
-                    return word.OriginalWord.ToLower().Contains(key);
-                });
-                if (found)
-                {
-                    suitableItems.Add(word.OriginalWord);
-                }
-            }
-            if (suitableItems.Count == 0)
-            {
-                suitableItems.Add("Не найдено");
-            }
-            sender.ItemsSource = suitableItems;
-        }
-        // Handle user selecting an item, in our case just output the selected item.
-        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
-        {
-            ttip.IsOpen = true;
-            var currentItem = args.SelectedItem.ToString();
-            Word GetWord()
-            {
-                Word tmpWord = new Word();
-                foreach (var word in Words.WordsArray)
-                {
-                    if (word.OriginalWord == currentItem)
-                    {
-                        tmpWord = word;
-                    }
-                }
-                return tmpWord;
-            }
-            ttip.Title = GetWord().OriginalWord;
-            ttip.Subtitle = GetWord().WordTranslation.ToString();
-            ttip.Target = mainAB;
-        }
-
-        private void GDic_Loaded(object sender, RoutedEventArgs e)
-        {
-            GDic.Background = GlobalUISettings.BgAcr;
         }
     }
 }
