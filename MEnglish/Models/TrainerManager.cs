9c18270d594985428735bc523933b1e9239e4f61
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2022-12-17 20:13:09 +0300

    add TrainerManager/TrainerManagerViewModel/TrainerManagerPage

---
+++ MEnglish/Models/TrainerManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEnglish.Models
+{
+    internal class TrainerManager
+    {
+    }
+}
