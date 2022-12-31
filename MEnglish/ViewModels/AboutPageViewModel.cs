b540d015769b4c25a1c11de04a587f0ab49c301c
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2022-12-31 21:00:41 +0300

    happy new year!

--- MEnglish/ViewModels/AboutPageViewModel.cs
+++ MEnglish/ViewModels/AboutPageViewModel.cs
@@ -9,7 +9,7 @@ namespace MEnglish.ViewModels
 {
     public class AboutPageViewModel : BaseViewModel
     {
-        public string LastUpdate { get; } = "Обновление от: 30.12.2022";
+        public string LastUpdate { get; } = "Обновление от: 31.12.2022";
         public string AppVersion { get; } = "Версия: 11.5 Store Edition";
         private int rectanleRotation;
         public int RectanleRotation
