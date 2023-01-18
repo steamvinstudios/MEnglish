976eeea8d5a8a356967c7bd35f6d93a98c0a933d
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2023-01-18 08:17:36 +0300

    update info

--- MEnglish/ViewModels/AboutPageViewModel.cs
+++ MEnglish/ViewModels/AboutPageViewModel.cs
@@ -9,7 +9,7 @@ namespace MEnglish.ViewModels
 {
     public class AboutPageViewModel : BaseViewModel
     {
-        public string LastUpdate { get; } = "Обновление от: 15.1.2023";
+        public string LastUpdate { get; } = "Обновление от: 18.1.2023";
         public string AppVersion { get; } = "Версия: 11.5 Store Edition";
         private int rectanleRotation;
         public int RectanleRotation
