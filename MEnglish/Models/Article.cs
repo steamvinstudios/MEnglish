254a4a2029f8419d365a62f28e4a5d1c31b4d7f5
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2023-06-03 18:26:59 +0300

    several fixes

---
+++ MEnglish/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEnglish.Models
+{
+    public class Article
+    {
+        public string Title { get; set; } = "No data";
+        public string Body { get; set; } = "No data";
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
